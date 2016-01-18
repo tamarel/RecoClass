6690d23cfc714596df8c75b082f4f98369c672bc
tamarel <tamareliyahou@gmail.com>
2016-01-18 18:12:59 +0200

    add parse

--- Sample/app/src/main/java/com/microsoft/projectoxford/face/samples/MainActivity.java
+++ Sample/app/src/main/java/com/microsoft/projectoxford/face/samples/MainActivity.java
@@ -44,7 +44,6 @@ import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity {
 
-
         private Button signButton,registerNowButton;
         private TextView userName,password,helloGuest;
         private EditText passwordField,userNameField;
@@ -54,7 +53,18 @@ public class MainActivity extends ActionBarActivity {
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main);
-            init();
+
+            signButton = (Button)findViewById(R.id.signButton);
+            registerNowButton = (Button)findViewById(R.id.registerButton);
+            userName = (TextView)findViewById(R.id.userNameLable);
+            password = (TextView)findViewById(R.id.passwordLable);
+            helloGuest =(TextView)findViewById(R.id.helloGuestLable);
+            passwordField=(EditText)findViewById(R.id.passwordFieldText);
+
+            userNameField=(EditText)findViewById(R.id.userNameFieldText);
+
+            logo=(ImageView)findViewById(R.id.logo);
+
             signButton.setOnClickListener(new View.OnClickListener() {
                 public void onClick(View v) {
 
@@ -67,19 +77,7 @@ public class MainActivity extends ActionBarActivity {
         }
 
 
-    private void  init (){
-        signButton = (Button)findViewById(R.id.signButton);
-        registerNowButton = (Button)findViewById(R.id.registerButton);
-        userName = (TextView)findViewById(R.id.userNameLable);
-        password = (TextView)findViewById(R.id.passwordLable);
-        helloGuest =(TextView)findViewById(R.id.helloGuestLable);
-        passwordField=(EditText)findViewById(R.id.passwordFieldText);
 
-        userNameField=(EditText)findViewById(R.id.userNameFieldText);
-
-        logo=(ImageView)findViewById(R.id.logo);
-
-    }
 
 
 
